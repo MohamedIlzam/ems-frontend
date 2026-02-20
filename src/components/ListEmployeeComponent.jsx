0fc6fb7a7d70c5756edd5ef0759e885bc7b450d6
MohamedIlzam <it24610792@my.sliit.lk>
2026-02-20 23:58:53 +0530

    List employeee method

---
+++ src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function    ListEmployeeComponent() {
+    return (
+        <div>
+            <h2 className="text-center">Employees List</h2>
+        </div>
+    )
+}
