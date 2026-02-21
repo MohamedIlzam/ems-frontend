ab2952629c7a6a329a9d0d9fdfbc25f450b67689
MohamedIlzam <it24610792@my.sliit.lk>
2026-02-21 21:51:08 +0530

    Test fake employee

--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -1,9 +1,57 @@
-import React from "react";
+import React from 'react'
 
-function    ListEmployeeComponent() {
-    return (
-        <div>
-            <h2 className="text-center">Employees List</h2>
-        </div>
-    )
+const ListEmployeeComponent = () => {
+
+        const dummyData=[
+            {
+                id:1,
+                firstName:"Rohit",
+                lastName:"Sharma",
+                email:"rohit@gmail.com"
+            },
+            {
+                id:2,
+                firstName:"Virat",
+                lastName:"Kohli",
+                email:"virat@gmail.com"
+            },
+            {
+                id:3,
+                firstName:"MS",
+                lastName:"Dhoni",
+                email:"msdhoni@gmail.com"
+            }
+        ]
+
+
+  return (
+    <div className='container'>
+        <h2 className='text-center'>List of Employees</h2>
+        <table className="table table-striped table-bordered"> 
+            <thead>
+                <tr>
+                    <th>Employee Id</th>
+                    <th>Employee First Name</th>
+                    <th>Employee Last Name</th>
+                    <th>Employee Email Id</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    dummyData.map(employee => ( 
+                        <tr key={employee.id}>
+                            <td>{employee.id}</td>
+                            <td>{employee.firstName}</td>
+                            <td>{employee.lastName}</td>
+                            <td>{employee.email}</td>
+                        </tr>
+                    ))
+                }
+            </tbody>
+        </table>
+
+    </div>
+  )
 }
+
+export default ListEmployeeComponent
